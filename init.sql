852cf47f7072794b18cc77ad91d2f718b4822a42
valya <moralek228@gmail.com>
2024-10-13 18:50:43 +0300

    Внёс правки в Dockerfile, вынес настройки бд в .env

--- init.sql
+++ init.sql
@@ -23,4 +23,4 @@ CREATE TABLE IF NOT EXISTS words (
 INSERT INTO users (username, password) VALUES ('testuser', 'password123');
 INSERT INTO dictionaries (name, description, user_id) VALUES ('My Dictionary', 'A test dictionary', 1);
 INSERT INTO words (word, translation_word, dictionary_id, word_language_code, translation_word_language_code)
-VALUES ('Hello', 'Привет', 1, 1, 2);
+VALUES ('Hel2l22222o', 'Привет2', 1, 1, 2);
