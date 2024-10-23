8c4a7c7104a1abf11785d50079cc266d5d87c8eb
n.gabitov <n.gabitov@analyticum.pro>
2024-10-23 10:27:16 +0500

    Добавил базовые проверки при создании словарика

--- internal/domain/dictionary.go
+++ internal/domain/dictionary.go
@@ -2,8 +2,8 @@ package domain
 
 type Dictionary struct {
 	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Name        string `json:"name" validate:"required,min=3,max=50"`
+	Description string `json:"description" validate:"max=255"`
 	UserID      int64  `json:"user_id"`
 }
 
--- internal/handler/dictionary_handler.go
+++ internal/handler/dictionary_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/valyamoro/internal/domain"
 	"net/http"
 )
@@ -16,12 +17,16 @@ type Dictionaries interface {
 
 type DictionaryHandler struct {
 	BaseHandler
+	Validator *validator.Validate
 	dictionariesService Dictionaries
 }
 
 func NewDictionaryHandler(dictionaries Dictionaries) *DictionaryHandler {
+	v := validator.New()
+
 	return &DictionaryHandler{
 		dictionariesService: dictionaries,
+		Validator: v,
 	}
 }
 
@@ -38,6 +43,11 @@ func (dh *DictionaryHandler) CreateDictionary(c *gin.Context) {
 	if err := c.ShouldBindJSON(&dictionary); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+	}
+
+	if err := dh.Validator.Struct(dictionary); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return 
 	}
 
 	_, err := dh.dictionariesService.Create(dictionary)
