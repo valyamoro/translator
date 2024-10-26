b6bd9d29406ea4e92fc5da981382aa234869af1a
valya <moralek228@gmail.com>
2024-10-26 10:49:33 +0300

    Добавил метод для сравнения юзеров

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -6,3 +6,21 @@ type User struct {
 	Password     string       `json:"password"`
 	Dictionaries []Dictionary `json:"dictionaries"`
 }
+
+func (u User) Equals(other User) bool {
+	if u.ID != other.ID || u.Username != other.Username {
+		return false 
+	}
+
+	if len(u.Dictionaries) != len(other.Dictionaries) {
+		return false 
+	}
+
+	for i := range u.Dictionaries {
+		if u.Dictionaries[i] != other.Dictionaries[i] {
+			return false 
+		}
+	}
+
+	return true
+}
